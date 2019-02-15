209982682916dc44125c57894dd4cfb37406b1b8
elyjh <elyjh@rose-hulman.edu>
2019-02-15 10:27:42 -0500

    k

--- src/m2_bank_account.py
+++ src/m2_bank_account.py
@@ -3,8 +3,8 @@
 # successful
 def main():
     # when you have initialized your object, use the calls below to test
-    #run_test_init()
-    #run_test_withdraw()
+    run_test_init()
+    run_test_withdraw()
     return
 
 
@@ -47,9 +47,12 @@ class Bank(object):
           :type account_number: str
         """
     # ---------------------------------------------------------------------
-    # TODO: 1. Implement and test instances of this class.
+    # DONE: 1. Implement and test instances of this class.
     #     See the testing code (scroll down near bottom) for more examples.
     # ---------------------------------------------------------------------
+        self.name = name
+        self.balance = initial_deposit
+        self.account_number = account_number
 
 
     def withdraw(self, amount):
@@ -84,6 +87,10 @@ class Bank(object):
     #   Put your code for withdraw below
     #
     # ---------------------------------------------------------------------
+        if self.balance - amount >= 0:
+            return self.balance - amount
+        else:
+            print("Insufficient Funds!")
 
 
 def run_test_init():
@@ -106,15 +113,59 @@ def run_test_init():
         print_failure_message()
     print()
     # ---------------------------------------------------------------------
-    # TODO: 2. Add two more test cases for your Bank class below.
+    # DONE: 2. Add two more test cases for your Bank class below.
+    # ---------------------------------------------------------------------
+    b2 = Bank('Bob', 5000000000, 'A2')
+    expected_name = 'Bob'
+    expected_balance = 5000000000
+    expected_account_number = 'A2'
+    print("Expected:", expected_name, expected_balance, expected_account_number)
+    print("Actual:  ", b2.name, b2.balance, b2.account_number)
+    if (expected_name == b2.name) and (expected_balance == b2.balance) and (
+            expected_account_number == b2.account_number):
+        print("Test passed SUCCESSFULLY!")
+    else:
+        print_failure_message()
+    print()
+
+    b3 = Bank('Boris', -2304, 'A3')
+    expected_name = 'Boris'
+    expected_balance = -2304
+    expected_account_number = 'A3'
+    print("Expected:", expected_name, expected_balance, expected_account_number)
+    print("Actual:  ", b3.name, b3.balance, b3.account_number)
+    if (expected_name == b3.name) and (expected_balance == b3.balance) and (
+            expected_account_number == b3.account_number):
+        print("Test passed SUCCESSFULLY!")
+    else:
+        print_failure_message()
+    print()
+
+    # ---------------------------------------------------------------------
+    # DONE: 3. Implement your test for the withdraw method below
     # ---------------------------------------------------------------------
 
-# ---------------------------------------------------------------------
-# TODO: 3. Implement your test for the withdraw method below
-# ---------------------------------------------------------------------
-def run_test_withdrawal():
-# Implement at least two tests.  Use copy and paste to speed your coding.
-    pass
+def run_test_withdraw():
+    # Implement at least two tests.  Use copy and paste to speed your coding.
+    b1 = Bank('Brackin', 10000, 'A1')
+    print("Expected:", 10000 - 2589)
+    print("Actual:  ", b1.withdraw(2589))
+    if b1.withdraw(2589) == (10000 - 2589):
+        print("Test passed SUCCESSFULLY!")
+    else:
+        print_failure_message()
+    print()
+    # ---------------------------------------------------------------------
+    # DONE: 2. Add two more test cases for your Bank class below.
+    # ---------------------------------------------------------------------
+    b2 = Bank('Bob', -235879, 'A2')
+    print("Expected:", "Insufficient Funds!")
+    print("Actual:  ", b2.withdraw(500))
+    if b2.withdraw(500) is None:
+        print("Test passed SUCCESSFULLY!")
+    else:
+        print_failure_message()
+    print()
 
 
 def print_failure_message():
